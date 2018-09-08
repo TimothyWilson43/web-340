5e0da4061ad88aa1d44d95559b16c23807616008
Timothy Wilson <tiwilson@my.bellevue.edu>
2018-09-07 22:25:28 -0500

    Adding assignment 6.3

---
+++ week-6/mongoose-connection/wilson-assignment-6.3.js
@@ -0,0 +1,25 @@
+ //require
+
+ var express = require('express');
+ var http = require('http');
+ var logger = require('morgan');
+ var mongoose = require('mongoose');
+
+ var mongoDB = 'mongodb://wilsonxchevy:Meatball11!!@ds249942.mlab.com:49942/ems';
+ mongoose.connect(mongoDB, {
+     useMongoClient: true
+ });
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB connection error: "));
+db.once('open', function(){
+    console.log('Application connected to mLab');
+});
+
+var app = express();
+app.use(logger('dev'));
+
+http.createServer(app).listen(3000, function(){
+    console.log('Application started and listening on port 3000');
+});
