d9a174db73a01c253c2643494beb1982b6c60887
Timothy Wilson <tiwilson@my.bellevue.edu>
2018-09-16 13:51:43 -0500

    Adding Assignment 7.3

---
+++ week-7/chai-example/wilson-fruits.js
@@ -0,0 +1,18 @@
+/*
+============================================
+; Title:  Assignment 7.3
+; Author: Timothy Wilson
+; Date:   16 Sep 2018
+; Modified By: Timothy Wilson
+; Description: This program demonstrates the
+; use of chai.
+;===========================================
+*/
+
+function fruits(str) {
+
+    return str.split(',');
+
+}
+
+module.exports = fruits;
