4114a3b1cd119faffe431b96501c4ad6abacdc83
Timothy Wilson <tiwilson@my.bellevue.edu>
2018-09-23 22:16:39 -0500

    Adding Assignment 8.4

--- ems/app.js
+++ ems/app.js
@@ -11,42 +11,16 @@
 var express = require("express");
 var http = require("http");
 var path = require("path");
-var helmet = require("helmet");
 var logger = require("morgan");
-var mongoose = require("mongoose");
+var helmet = require("helmet");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 var csrf = require("csurf");
+var mongoose = require("mongoose");
+var Employee = require("./models/employee");
 
 // CSURF Protection setup
 var csrfProtection = csrf({ cookie: true });
-
-var app = express();
-
-app.set("views", path.resolve(__dirname, "views"));
-app.set("view engine", "ejs");  
-app.use(logger("short"));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(cookieParser());
-app.use(csrfProtection);
-app.use(function (request, response, next) {
-    var token = request.csrfToken();
-    response.cookie('XSRF-TOKEN', token);
-    response.locals.csrfToken = token;
-    next();
-});
-app.get("/", function(request, response) {
-    response.render("index", {
-        message: "New Employee Entry Page"
-    });
-});
-
-app.post("/process", function(request, response) {
-    console.log(request.body.txtName);
-    response.redirect("/");
-});
 
 // mLab connection
 var mongoDB = "mongodb://wilsonxchevy:Meatball11!!@ds249942.mlab.com:49942/ems";
@@ -60,48 +34,89 @@ db.once("open", function () {
     console.log("Application connected to mLab MongoDB instance");
 });
 
-
-// application
 var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 8080);
+
 app.use(logger("short"));
-
-//calling views
-
-
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
+app.use(csrfProtection);
+app.use(helmet.xssFilter());
+app.use(function (request, response, next) {
+    var token = request.csrfToken();
+    response.cookie('XSRF-TOKEN', token);
+    response.locals.csrfToken = token;
+    next();
+});
 app.get("/", function (request, response) {
     response.render("index", {
-        title: "Home page"
-    });
-});
-
-app.get("/index", function (request, response) {
-    response.render("index", {
-        title: "Home page"
-    });
-});
-
-app.get("/list", function (request, response) {
-    response.render("list", {
-        title: "Employee List"
+        message: "New Employee Entry Page"
     });
 });
 
 app.get("/new", function (request, response) {
     response.render("new", {
-        title: "New Employee Page"
+        title: "New Employee"
     });
-
 });
 
+app.post("/process", function (request, response) {
+    if (!request.body.txtName) {
+        response.status(400).send("Entries must have a name");
+        return;
+    }
 
-app.get("/", function (request, response) {
+    var employeeName = request.body.txtName;
+    console.log(employeeName);
 
-    response.render("index", {
-
-        message: "XSS Prevention Example"
-
+    var employee = new Employee({
+        name: employeeName
     });
 
+    employee.save(function (error) {
+        if (error) throw error;
+
+        console.log(employeeName + " success!");
+    });
+
+    response.redirect("/list");
+});
+
+app.get("/list", function (request, response) {
+    Employee.find({}, function (error, employee) {
+        if (error) throw error;
+
+        response.render("list", {
+            title: "Employee List",
+            employees: employees
+        });
+    });
+});
+
+app.get("/view/:queryName", function (request, response) {
+    var queryName = request.params.queryName;
+
+    Employee.find({ 'name': queryName }, function (error, employees) {
+        if (error) throw error;
+
+        console.log(employees);
+
+        if (employees.length > 0) {
+            response.render("view", {
+                title: "Employee Record",
+                employee: employees
+            })
+        }
+        else {
+            response.redirect("/list")
+        }
+
+    });
 });
 
 // create server
